97563597b665899b4566a78b87154690a9f20335
Thushar <tthushar2004@gmail.com>
2024-01-21 23:59:29 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Joke generator',
       theme: ThemeData(),
       home: const Home(),
       debugShowCheckedModeBanner: false,
